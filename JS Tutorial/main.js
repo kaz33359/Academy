cefd3a653da08dd4c4f512bc148f54f0c86c9fdf
Kaz <40711181+kaz33359@users.noreply.github.com>
2021-06-04 11:14:10 +0530

    Files Add

---
+++ JS Tutorial/main.js
@@ -0,0 +1,172 @@
+// // alert("hello"); 
+// // console.log("hello");
+// const firstName = "Kaz" ;
+// const lastName = "Kazuki" ;
+// const phone = "7356563359";
+
+
+// // ======== Template String way ========
+// let result = `hi ${firstName} ${lastName} your Mobile number is ${phone}` ;
+// // console.log(result);
+
+// // ======== Html  Template ========
+// let html = `
+// <h1> ${firstName} </h1> 
+// <p> ${lastName}</p>
+// <span>${phone} </span>
+// `;
+
+// console.log(html)
+
+// Control FLow
+// const test = ["wachi", "amal", "Kaz"]; //Aray inizialization
+// let i = 0; // Variable Diclaration
+
+        // For Loop
+
+// console.log("For Test");
+// for(let i =  0; i <= 5; i++){
+//     console.log(i)
+// }
+// console.log("Loop End")
+
+// for ( i = 0; i < test.length; i++) {
+//     // console.log(test[i]);
+//     let html = `<div><h1>${test[i]}</h1></div>`
+//     console.log(html)
+// }
+
+        // WHile loop
+
+// console.log("While Test")
+// while (i < test.length) {
+//     console.log(test[i]);
+//     i++;
+// }
+
+        // Do WHile loop
+
+// console.log("Do While Test")
+// do{
+//     console.log(test[i]);
+//     i++;
+// }while(i < test.length)
+
+        // IF Sattement
+// const age = 20;
+// const pass = 'P@ssword';
+
+// if (age > 20) {
+//     console.log("You Are  Old")
+// }else{
+//     console.log("You Are Not That old")
+// }
+
+// if (test.length>3) {
+//     console.log("Too Long");
+// } else {
+//     console.log("Not Too Long");
+
+// }
+
+// if (pass.length >= 8 && pass.includes('@')) {
+//     console.log("Password is Perfect");
+// } else if (pass.length >= 8 || pass.includes('@')) {
+//     console.log("ok Long");
+// }else{
+//     console.log("Bad Password");
+// }
+
+
+
+// // ======== Functions ========
+
+// Function Declaration
+
+// function greet(){
+//     console.log("Hello World!!");
+//     return ;
+// }
+
+// greet(); //Function call
+
+// Function Expression ~ Stores function insodes a variable
+
+// const speak = function (){
+//     console.log("Hello World!!");
+// };
+
+// speak(); //Function call
+
+// const speak = () => 'Hello World!!';
+// const res = speak();
+// console.log(res); 
+// arrow function
+
+// const carea = (radius) => {
+//     return 3.15 * radius**2;
+// };
+// const area =  carea(10);
+// console.log(`area is ${area}`);
+
+
+// ======== DOM Manupulation  ========
+
+// const para = document.querySelector('p');
+// console.log(para);
+
+// const err = document.querySelector(".error");
+// console.log(err);
+
+// const div = document.querySelector("div.error");
+// console.log(div);
+
+// const paraAll = document.querySelectorAll("p");
+// console.log(paraAll);
+// paraAll.forEach(pra =>{
+//     console.log(pra);
+// });
+
+
+// Element by iD
+// const title = document.getElementById('title');
+// console.log(title);
+
+// Element by class name
+// const errors = document.getElementsByClassName("error");
+// console.log(errors);
+
+// Element by Tag Name
+// const ps = document.getElementsByTagName('p')
+// console.log(ps);
+
+
+// const para = document.querySelector('p')
+// console.log(para.innerText);
+
+// para.innerText = "Hello World changes"  // +=
+
+// const para = document.querySelectorAll('p');
+
+// para.forEach((para) => {
+//   console.log(para.innerText);
+//   para.innerText += 'New text'
+// });
+
+// const content = document.querySelector( '.content');
+// console.log(content.innerHTML);
+// content.innerHTML = '<h2>Changed to h2</h2>' // +=
+
+// const people = ['mario','romio','romario'];
+// people.forEach ( person => {
+//     content.innerHTML += `<h3> ${person}</h3>`
+// })
+
+const link = document.querySelector('a')
+console.log(link.getAttribute('href'));
+link.setAttribute("href", "https://thedaxads.com/");
+
+const msg = document.querySelector(".error");
+console.log(msg.getAttribute('class'));
+msg.setAttribute("class", "success");
+msg.setAttribute("style", "color:green");
---
+++ Tutorial/assets/js/main.js
@@ -0,0 +1,111 @@
+
+// alert(test);
+
+// let a; // Declaration
+// a = "42.5"; // Initiliazion
+// console.log(a);
+// const a= 10;
+// console.log("a= "+a);
+// a=20;
+// console.log("a= " + a);
+
+// let b = 10;
+// console.log("b=" + b);
+// b=20;
+// console.log("b=" + b);
+
+// let firstname = prompt("Firstname");
+// let Lastname= prompt("Lastname");
+// console.log("Hello "+ firstname+ " "+ Lastname);
+ 
+
+// let num1= prompt("num 1")
+// let num2= prompt("num 2")
+// console.log(num1+num2);
+
+let a = "10";
+let b = "15";
+// console.log(a == b);
+
+// let pes = {
+//     name : "kaz",
+//     number : 456789123
+// };
+
+let names = ['kaz',2,'vipin','shifa','vipin'];
+let names2 = ['kazs',2,'vipina','shifad','vipinf'];
+// console.log(names);
+// console.log(typeof(names)); 
+
+// console.log(pes);
+// let x = names.lastIndexOf('vipin')
+// console.log(names.lastIndexOf("vipin"));
+// console.log(concat(names, names2));
+
+function add (a,b){
+    let c = a + b; 
+    return c;
+    // return a + b
+}
+
+// console.log("addition " +  add(5,6));
+
+// add(5, 6);
+
+// let day=1;
+// while(day>8){
+
+//     //
+
+//     day++;
+
+// }
+// let marks = prompt("Enter Marks ");
+
+// if (marks > 0 && marks <= 25) {
+//     console.log("Failed");
+
+// } else if (marks > 25 && marks <= 50) {
+//     console.log("Good");
+
+// }else if (marks > 50 && marks <= 75) {
+//     console.log("Average");
+
+// }else if (marks > 75 && marks <= 100) {
+//     console.log("Excelent");
+// }
+// else{
+//     console.log("enter correct mark between 0 nd 100");
+// }
+
+// program for a simple calculator
+// let result;
+
+// take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+// let result = 0;
+// const operater = prompt("+ - * / : ")
+// const number1 = parseInt(prompt('Enter first number: '));
+// const number2 = parseInt(prompt('Enter second number: '));
+
+// switch (operater) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(result);
+//         break;
+
+//     case '-':
+//         result = number1 - number2;
+//         console.log(result);
+//         break;    
+
+
+//     default:
+//         console.log("Correct Operation Required");
+// }
+function submit(){
+    alert("hello");
+}
+
